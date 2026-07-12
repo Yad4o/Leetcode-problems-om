0ddb2cb1733f696de1b97de72bcce8e8d2bc9d36
Om yadao <omyadao1706@gmail.com>
2026-07-12 22:48:35 +0530

    Time: 2 ms (48.56%), Space: 12.8 MB (73.87%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def matrixReshape(self, mat, r, c):
-        """
-        :type mat: List[List[int]]
-        :type r: int
-        :type c: int
-        :rtype: List[List[int]]
-        """
         rows = len(mat)
         cols = len(mat[0])
         if rows * cols != r * c:
