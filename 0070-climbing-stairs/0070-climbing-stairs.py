3fda95cfc4175d1c6faa6daf6ddd2f98242eaaf7
Om yadao <omyadao1706@gmail.com>
2026-07-15 17:41:50 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,3 +7,8 @@ class Solution(object):
         for i in range (2, n + 1):
             a , b = b , a + b
         return b
+# class Solution(object):
+#     def climbStairs(self, n):
+#         if n == 1 or n == 0:
+#             return 1
+#         return self.climbStairs(n - 1) + self.climbStairs(n - 2)
