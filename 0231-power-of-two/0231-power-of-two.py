3340f42e20027f4ec5205487fcba364ddb00f4b2
Om yadao <omyadao1706@gmail.com>
2026-07-15 18:11:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.18%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def isPowerOfTwo(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
         if n == 1:
             return True
         if n == 0 or n % 2 != 0:
