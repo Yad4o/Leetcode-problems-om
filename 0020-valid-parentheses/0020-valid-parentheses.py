89e6d4a39b58a88b4f36a30015e0511a6a57efab
Om yadao <omyadao1706@gmail.com>
2026-07-28 20:37:32 +0530

    Time: 3 ms (73.28%), Space: 12.5 MB (70.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,8 +8,7 @@ class Solution(object):
         for x in s:
             if x == '(' or x == '[' or x == '{':
                 A.append(x)
-            
-            else: 
+            else:
                 if len(A) == 0:
                     return False
                 elif x == ')' and A[-1] != '(':
