0221ad9f9010f349c4733f69734f30a94b52f003
Om yadao <omyadao1706@gmail.com>
2026-08-03 22:02:52 +0530

    Time: 2 ms (88.02%), Space: 13.3 MB (57.72%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,10 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        total = sum(nums)
+        left = 0
         for i in range(len(nums)):
-            left = sum(nums[:i])
-            right = sum(nums[i+1:])
+            right = total - left - nums[i]
+            
 
             if left == right:
                 return i
+            left += nums[i]
         return -1
