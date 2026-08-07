719a9a00a0d9342d058a47b91c74b73fb12ec67e
Om yadao <omyadao1706@gmail.com>
2026-08-07 23:27:12 +0530

    Time: 0 ms (100%), Space: 12.3 MB (77.24%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i = 0
+        j = 0
+        m = len(s)
+        n = len(t)
+        if m < 0:
+            return True
+        while i < m and j < n:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        if i == m:
+            return True
+        return False
