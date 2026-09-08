6c233396a9fc4ce1773b1e74b6073179a41b4b85
Om yadao <omyadao1706@gmail.com>
2026-09-08 22:46:39 +0530

    Time: 2 ms (60.5%), Space: 12.4 MB (58.98%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,22 +1,14 @@
 class Solution(object):
     def simplifyPath(self, path):
-        """
-        :type path: str
-        :rtype: str
-        """
         stack = []
-        
-        # Split by '/' automatically removes duplicate slashes (creates empty strings)
-        components = path.split('/')
-        
-        for portion in components:
-            if portion == '..' or portion == '':
-                # If '..', go up one level by popping from stack (if stack isn't empty)
-                if portion == '..' and stack:
+
+        for folder in path.split("/"):
+            if folder == "" or folder == ".":
+                continue
+            elif folder == "..":
+                if stack:
                     stack.pop()
-            elif portion != '.':
-                # Any normal directory name (including '...', '....', 'a.') goes into the stack
-                stack.append(portion)
-                
-        # Join components with '/' and ensure it starts with a leading '/'
+            else:
+                stack.append(folder)
+
         return "/" + "/".join(stack)
