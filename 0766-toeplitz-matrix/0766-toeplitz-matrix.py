d86bf45e83fad19215fa3dcd41892b7ad0a56dda
Om yadao <omyadao1706@gmail.com>
2026-09-13 23:03:28 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.68%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        for r in range(1, len(matrix)):
+            for c in range(1, len(matrix[0])):
+                
+                if matrix[r][c] != matrix[r - 1][c - 1]:
+                    return False
+
+        return True
